97bdeb085e1da11aa05abc6b26d0a8d4177fe3fb
Pavel Bobkov <>
2023-03-05 18:17:43 +0300

    read parameters from psp

--- project_1/project.asm
+++ project_1/project.asm
@@ -2,9 +2,47 @@ code_seg segment
     ASSUME CS:code_seg,DS:code_seg,ES:code_seg
     org 100h
 include macros.lib
+
 ;=============================== Start ===============================
 start:
-    
+
+;-------------------------- Check Parameters -------------------------
+
+check_parameters:
+    mov SI, 80h
+    mov AL, byte ptr[SI]
+    cmp AL, 0
+    je ask_filenames
+    jmp read_psp_parameters
+
+;---------------------------- Ask Filenames --------------------------
+
+ask_filenames:
+    _new_line_
+    _print_message_ 'Input filename for read > '
+    mov AH, 0Ah
+    lea DX, NameReadFile
+    int 21h
+    xor BH, BH
+    mov BL, NameReadFile[1]
+    mov NameReadFile[BX+2], 0
+
+    _new_line_
+    _print_message_ 'Input filename for write > '
+    lea DX, NameWriteFile
+    int 21h
+    mov BL, NameWriteFile[1]
+    mov NameWriteFile[BX+2], 0
+
+;-----------------------  Read PSP Parameters --------------------------
+
+read_psp_parameters:
+    inc SI
+    _read_psp_parameters_ NameReadFile, Space
+    inc SI
+    _read_psp_parameters_ NameWriteFile, CR
+
+;---------------------------------------------------------------------
 
 int 20h
 
@@ -18,6 +56,9 @@ int 20h
     CR      EQU 0Dh
     LF      EQU 0Ah
     Space   EQU 20h
+    
+    NameReadFile    DB 14,0,14 dup (0)
+    NameWriteFile   DB 14,0,14 dup (0)
 
 ;=====================================================================
 
